18f43933985f44ab9ebdfc69cb92c6397f462284
Ben Yoon <BYoon12@gmail.com>
2022-12-08 00:01:05 -0500

    fixed error

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,9 +1,5 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass").Vehicle
-
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+const VehicleModule = require("./vehicle").Vehicle
 
 class Car extends VehicleModule {
     constructor(make, model, year, color, mileage) {
